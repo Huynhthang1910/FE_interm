33b7688430747fbc8eccece4a56ec386dfb39c25
Huynhthang19 <thuanthang1910@gmail.com>
2023-04-05 12:46:57 +0700

    a

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import * as ReactDOMClient from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+
+      <App />
+
+    </BrowserRouter>
   </React.StrictMode>
 );
 
