8da9866850f8fab3ed50158047022b6d326ea319
Nguyễn Hoàng Minh <83760061+vluit17@users.noreply.github.com>
2023-04-15 17:49:40 +0700

    them route contact bỏ dashboard, team,....

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
 
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
@@ -43,13 +44,14 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar isSidebar={isSidebar} id={isAdmin} userid={isID} />
+          <Sidebar isSidebar={isSidebar} ad={isAdmin} userid={isID} />
+          {console.log(isAdmin, isID)}
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              <Route path="/" element={<Dashboard />} />
+              <Route path="/" element={<Calendar />} />
               <Route path="/team" element={<Team />} />
-
+              <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
