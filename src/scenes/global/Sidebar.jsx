127d3267549f63af431f7ad446d57818e2a4d798
vluit17 <minh2002hoang@gmail.com>
2023-04-19 16:39:28 +0700

    update create acccount sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -163,7 +163,7 @@ const Sidebar = ({ ad, userid }) => {
                 <Item
                   title="Headquarters"
                   to="/team"
-                  icon={<PeopleOutlinedIcon />}
+                  icon={<PersonOutlinedIcon />}
                   selected={selected}
                   setSelected={setSelected}
                 />
