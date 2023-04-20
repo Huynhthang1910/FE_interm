6da34b82fa60ba530021654a34c164a1fda16402
Ngọ Văn Long <75620455+Thuandiepbt1707@users.noreply.github.com>
2023-04-20 13:10:55 +0700

    complete updatHeadquarter
    Hoàn thiện phần cập nhận trường thông tin trụ sở

--- src/scenes/team/UpdateHeadquarter/UpdateHeadquarter.jsx
+++ src/scenes/team/UpdateHeadquarter/UpdateHeadquarter.jsx
@@ -1,13 +1,48 @@
-import { useState } from "react"
-import "./UpdateHeadquarter.scss"
+import { useState } from "react";
+import UrbanLogo from "./UrbanLogo.png";
+import "./UpdateHeadquarter.scss";
+import { Message } from "@mui/icons-material";
 
 const UpdateHeadquarter = (props) => {
+    const token = sessionStorage.getItem("token");
     const [inforHeadquarter, setInfor] = useState(props.inforHeadquarter);
-    console.log(inforHeadquarter)
+    // console.log(inforHeadquarter)
+    const changeHeadquarter = (e) => {
+        let tar = e.target;
+        let keyName = tar.name;
+        let value = tar.value;
+        console.log(keyName)
+        console.log(value)
+        setInfor({...inforHeadquarter,[keyName]:[value]})
+    }
     const callAPI = () => {
-        let payLoad = {
-            "headquarterName": inforHeadquarter.headquarterName,
-            "headquarterAddress": inforHeadquarter.headquarterAddress
+        if(String(inforHeadquarter.headquarterName)== "" || String(inforHeadquarter.headquarterAddress) == "") {
+            alert("Something is null! please check it again!")
+        }else{
+            let url = `https://be-intern.onrender.com/api/v2/headquarter/${inforHeadquarter.headquarterId}/update`
+            let payLoad = {
+                "headquarterName": String(inforHeadquarter.headquarterName),
+                "headquarterAddress": String(inforHeadquarter.headquarterAddress)    
+                }
+            let option = {
+                method: "PUT",
+                body: JSON.stringify(payLoad),
+                headers: {
+                    Authorization: `Bearer ${token}`, // Add the token as a bearer token
+                    "Content-Type": "application/json",
+                },
+            }
+            fetch(url, option)
+                .then((res) => res.json())
+                .then((data) => {
+                    if(data.message == "Cập Nhật Thành Công"){
+                        alert('Success! Please click "OK" to reload data!')
+                        props.resetWindows()
+                    } else{
+                        alert("Update failed!");
+                        console.log(data)
+                    }
+                    })
         }
     }
     return (
@@ -16,11 +51,32 @@ const UpdateHeadquarter = (props) => {
           className="form_around"
           onClick={()=>{props.handleSetInforHeadquarter(false)}}>
         </div>
-        <form>
-            <label>
+        <form className="form_headquarter">
+            <img src={UrbanLogo} alt="logo" className="form_headquarter__img" />
+            <label className="form_headquarter__title">
                 Headquarter Name
             </label>
-            <input type="text" />
+            <input 
+                type="text" 
+                name="headquarterName"
+                className="form_headquarter__input"
+                value={inforHeadquarter.headquarterName}
+                onChange={(e)=>{changeHeadquarter(e)}}/>
+            <label className="form_headquarter__title">
+                Headquarter Address
+            </label>
+            <input 
+                type="text" 
+                name="headquarterAddress"
+                className="form_headquarter__input"
+                value={inforHeadquarter.headquarterAddress}
+                onChange={(e)=>{changeHeadquarter(e)}}/>
+            <button 
+                type="button"
+                className="form_headquarter__btn"
+                onClick={()=>{callAPI()}}>
+                Update Headquarter
+            </button>
         </form>
     </>
    ) 
